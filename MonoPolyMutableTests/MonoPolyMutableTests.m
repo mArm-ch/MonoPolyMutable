4a9489cad1476c551befe969c994cc2ad0dda3c8
David Ansermot <mArm@Natsu.local>
2013-03-04 19:40:10 +0100

    Added some tests

--- MonoPolyMutableTests/MonoPolyMutableTests.m
+++ MonoPolyMutableTests/MonoPolyMutableTests.m
@@ -8,25 +8,68 @@
 
 #import "MonoPolyMutableTests.h"
 
+#import "MonomorphicMutableArray.h"
+#import "MonomorphicMutableDictionary.h"
+#import "PolymorphicMutableArray.h"
+#import "PolymorphicMutableDictionary.h"
+
 @implementation MonoPolyMutableTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     
     // Set-up code here.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Tear-down code here.
     
     [super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in MonoPolyMutableTests");
+
+- (void)testMonomorphicMutableArray {
+    
+    MonomorphicMutableArray *typedArray = [[MonomorphicMutableArray alloc] initWithClass:[UILabel class]];
+    
+    UILabel *label = [[UILabel alloc] init];
+        
+    STAssertNoThrow([typedArray addObject:label], @"");
+    STAssertNoThrow([typedArray insertObject:label atIndex:1], @"");
+    STAssertThrows([typedArray addObject:[NSNumber numberWithInt:1]], @"");
+    STAssertThrows([typedArray insertObject:[NSNumber numberWithInt:1] atIndex:2], @"");
+    STAssertTrue(([typedArray count] == 2), @"");
+
+    // Test initilizers exceptions throwing
+    STAssertThrows([[MonomorphicMutableArray alloc] init], @"");
+    STAssertThrows([[MonomorphicMutableArray alloc] initWithArray:[NSArray arrayWithObject:@""]], @"");
+    STAssertThrows([[MonomorphicMutableArray alloc] initWithArray:[NSArray arrayWithObject:@""] copyItems:YES], @"");
+    STAssertThrows([[MonomorphicMutableArray alloc] initWithCapacity:2], @"");
+    STAssertThrows([[MonomorphicMutableArray alloc] initWithCoder:nil], @"");
+    STAssertThrows([[MonomorphicMutableArray alloc] initWithContentsOfFile:@""], @"");
+    STAssertThrows([[MonomorphicMutableArray alloc] initWithContentsOfURL:nil], @"");
+                    
+}
+
+
+- (void)testMonomorphicMutableDictionary {
+    
+    MonomorphicMutableDictionary *typedDico = [[MonomorphicMutableDictionary alloc] initWithClass:[NSString class]];
+    
+}
+
+
+- (void)testPolymorphicMutableArray {
+    
+    PolymorphicMutableArray *typedArray = [[PolymorphicMutableArray alloc] initWithClass:[NSString class]];
+    
+}
+
+
+- (void)testPolymorphicMutableDictionary {
+    
+    PolymorphicMutableDictionary *typedDico = [[PolymorphicMutableDictionary alloc] initWithClass:[NSString class]];
+    
 }
 
 @end
